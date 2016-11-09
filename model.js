2e4d9c7fd5c431041b9920774d879ead309b6eb1
Adawat Chanchua <prophecyx@hotmail.com>
2016-11-09 09:55:29 +0700

    Deleted old source code

--- model.js
+++
@@ -1,9 +0,0 @@
-
-OnLoad = function() {
-
-    $(".signatureSpace").text("(____________________)");
-    $(".dName").text("[FirstName] [LastName] 01");
-    $(".dPosition").text("Position 01");
-    $(".ePosition").text("Position 02");
-    $(".eTel").text("Tel: ______________________");
-}
