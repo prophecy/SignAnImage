ec40f144f03028755325ecfdb58f3c5a1b01ee38
Adawat Chanchua <prophecyx@hotmail.com>
2016-11-16 13:44:57 +0700

    Set text fields by config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+getConfiguration = function() {
+
+    var root = {};
+
+    root.manager_name = "[Manager Name]";
+    root.manager_dept = "[Manager Dept]";
+
+    root.position = "[Your Position]";
+    root.tel = "[Your Tel]";
+
+    return root;
+}
